bd1f2ab67301c42033d876b4de2bebb428d341ab
MatValdejao <matvaldejao123@gmail.com>
2022-10-12 18:46:09 -0400

    getdb and postdb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,6 @@
 import "./form";
 import "./submit";
-import { initdb } from "./database";
+import { initdb, getdb, postdb } from "./database";
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,7 +10,8 @@ import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
 window.addEventListener("load", function () {
-	initdb();
+    initdb();
+    getdb();
 	document.getElementById("logo").src = Logo;
 	document.getElementById("bearThumbnail").src = Bear;
 	document.getElementById("dogThumbnail").src = Dog;
